181bf069b25472d71af36176bd0ac9353e6d6f38
Ross Corliss <rcorliss@mit.edu>
2024-10-04 13:36:47 -0400

    reduced minimum attenuator motion

--- bin/variableDictionaryXCD2.py
+++ bin/variableDictionaryXCD2.py
@@ -8,7 +8,8 @@ varTuning = {
 'Phi':[0.1,0.001],
 'ThetaS':[0.01,0.001],
 'ThetaL':[0.02,0.001],
-'Attenuator':[0.02,0.005],
+#'Attenuator':[0.02,0.005],
+'Attenuator':[0.005,0.003],
 'Dogleg':[0.01,0.001]
 }
 
