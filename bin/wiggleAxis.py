20a905d725ccfbeade67bcfbac28d1513802e2e3
Seth Howell <64179490+sothcode@users.noreply.github.com>
2024-10-16 16:41:09 -0400

    Update wiggleAxis.py

--- bin/wiggleAxis.py
+++ bin/wiggleAxis.py
@@ -1,7 +1,9 @@
 #! /usr/bin/python3
 
+from quickReport import readback
 from goto import goto
 from changeAxisDogleg import changeAxis
+from variableDictionaryXCD2 import varInterfaceAddresses as ADDR
 import sys
 
 
@@ -16,14 +18,24 @@ def _reverseLookup(dict,val):
     return key  
 
 
-def wiggleAxis(axis, step=0.01):
-    
+def wiggleAxis(axis, step=0.01, loop=True):
+    position = readback(ADDR['FPOS'])
+    dest1 = position - step
+    dest2 = position + step
+    while loop:
+        goto(axis, dest1)
+        goto(axis, dest2)
     return
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         axis=sys.argv[1]
         wiggleAxis(axis)
+    elif len(sys.argv) == 3:
+        axis=sys.argv[1]
+        step=sys.argv[2]
+        wiggleAxis(axis, step)
     else:
-        print("wiggleAxis.py failed.  Incorrect number of arguments")
+        print("wiggleAxis.py failed.  Incorrect number of arguments.")
         sys.exit()
