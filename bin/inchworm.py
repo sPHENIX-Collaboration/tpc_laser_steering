d6ad1b42edce2a8d5dad64e58af322c4f3451881
Seth Howell <64179490+sothcode@users.noreply.github.com>
2024-10-16 16:41:00 -0400

    Create inchworm.py

---
+++ bin/inchworm.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python3
+
+import sys
+from quickReport import readback
+from goto import goto
+from changeAxisDogleg import changeAxis
+from variableDictionaryXCD2 import varInterfaceAddresses as ADDR
+
