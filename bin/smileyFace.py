b12a4458213fd71937c406580dad25154924c609
Seth Howell <64179490+sothcode@users.noreply.github.com>
2024-10-23 17:40:04 -0400

    Create smileyFace.py
    Will draw smiley face on TPC central membrane

---
+++ bin/smileyFace.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python3
+#
+# smileyFace.py draws a smiley face on the TPC central membrane
+
+from quickReport import readback
+from goto import goto
+from changeAxisDogleg import changeAxis
+from variableDictionaryXCD2 import varInterfaceAddresses as ADDR
+import sys
+
+def leftEye():
+
+
+    return
+
+
+
+def rightEye():
+    
+
+
+    return
+
+def smile():
+    
+
+    return
+
+
+def smileyFace():
+
+
+    return
