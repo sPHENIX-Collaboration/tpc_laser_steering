5c34042e816b63620664703b5ef69443c417c037
Seth Howell <64179490+sothcode@users.noreply.github.com>
2024-10-30 11:11:18 -0400

    Update coordinateSearch.py
    building up

--- bin/coordinateSearch.py
+++ bin/coordinateSearch.py
@@ -4,22 +4,36 @@ import sys
 import os
 from quickAssign import writeXCD2
 from quickReport import reportXCD2
-from backAndForth import backAndForth
+from changeAxisDogleg import changeAxis
+from goto import goto
 from variableDictionaryXCD2 import varInterfaceAddresses as ADDR
 
-def coordinateSearch(axis, lb_PH=None, hb_PH=None, step_PH, lb_TH=None, hb_TH=None, step_TH):
+def coordinateSearch(axis, lb_PH=None, hb_PH=None, numPts_PH, lb_TH=None, hb_TH=None, numPts_TH):
 
     phiAxis = axis+"_PH"
     thsAxis = axis+"_TH_S"
     thlAxis = axis+"_TH_L"
 
     didChange = changeAxis()
-    backAndForth(phiAxis, )
+    (phiAxis, )
     didChange = changeAxis()
     goto()
-    backAndForth(thsAxis)
+    (thsAxis, )
 
     return
 
 if __name__ == "__main__":
-    if len(sys.argv) == 
+    if len(sys.argv) == 4:
+        axis=sys.argv[1]
+        coordinateSearch(axis, )
+    elif len(sys.argv) == 8:
+        axis=sys.argv[1]
+        lb_PH=sys.argv[2]
+        hb_PH=sys.argv[3]
+        numPts_PH=sys.argv[4]
+        coordinateSearch(axis,)
+    else:
+        print("coordinateSearch.py NOT EXECUTED.  Incorrect number of arguments.  Correct usage is:")
+        print("     ./coordinateSearch.py  [axis]  [numPts_PH]  [numPts_TH]       OR")
+        print("     ./coordinateSearch.py  [axis]  [lb_PH]  [hb_PH]  [numPts_PH]  []  []  []")
+        sys.exit()
