00d60c89a1dda9b9464e2fd877fe0e9247f642bc
Ross Corliss <rcorliss@mit.edu>
2024-10-31 17:00:19 -0400

    adding fun toys

--- bin/aimAtCentralMembrane.py
+++ bin/aimAtCentralMembrane.py
@@ -18,9 +18,9 @@ def aimAtCentralMembrane(laserName, x=0,y=0):
     zo=1050
 
     #theta angle is the triangle in the phi direction, so 
-	base=sqrt((x-xo)**2+(y-yo)**2)
-	height=zo-z
-	theta=atan(base/height)
+    base=sqrt((x-xo)**2+(y-yo)**2)
+    height=zo-z
+    theta=atan(base/height)
 
     #phi angle is the target phi:
 	horiz=(x-xo)
