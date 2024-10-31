0760371003843bf2c99781a49ee99841dcb98165
Seth Howell <64179490+sothcode@users.noreply.github.com>
2024-10-31 12:59:09 -0400

    Update makeLaserLogs.py
    added /dev/tty*

--- bin/makeLaserLogs.py
+++ bin/makeLaserLogs.py
@@ -5,6 +5,7 @@ from variableDictionaryXCD2 import varInterfaceAddresses as ADDR
 from variableDictionaryXCD2 import varUniqueID as AXID
 
 enr = 6.103515625e-5
+pathPC = '/mnt/c/Users/smh28/Documents/Github/tpc_laser_steering/bin/axisLogs/'
 path = '/home/pi/XCDCommandCodes/bin/axisLogs/'
 suffix = '.log'
 
@@ -13,7 +14,7 @@ for axisName in AXID:
     filename = path + axisName + suffix
     with open(filename, "w") as file:
 
-        file.write('{\"name\":%s' % (axisName))
+        file.write('{\"name\":\"%s\",\"USBport\":\"%s\"' % (axisName, "/dev/ttyUSB0"))
     
         for varName, varVal in ADDR.items():
             file.write(',\"%s\":%s' % (varName, 0.0))
