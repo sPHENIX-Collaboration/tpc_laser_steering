6dec853853f9141456066d9d06c666c214f7cf2b
Ross Corliss <rcorliss@mit.edu>
2024-10-31 16:56:08 -0400

    adding fun toys

---
+++ bin/aimAtCentralMembrane.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+# This is designed to tell a particular egg to steer to a given (x,y) coordinate pair on the central membrane
+
+from aimAt import aimAt
+import sys
+import math
+    
+
+
+def aimAtCentralMembrane(laserName, x=0,y=0):
+    x=float(x)
+    y=float(y)
+    z=0
+    #assume we have phi and theta in degrees
+    xo=-510
+    yo=160
+    zo=1050
+
+    #theta angle is the triangle in the phi direction, so 
+	base=sqrt((x-xo)**2+(y-yo)**2)
+	height=zo-z
+	theta=atan(base/height)
+
+    #phi angle is the target phi:
+	horiz=(x-xo)
+	vert=(y-yo)
+	phi=atan2(vert,horiz)
+
+    aimAt(laserName, laserName, theta, phi+180)
+    return
+
+
+
+
+
+
+if __name__ == "__main__":
+    #check args
+    if len(sys.argv)==4:
+        #assume (eggDbName,theta,phi).
+        #TODO:  specify which laser (port,axis), and which egg?  Or should we mate those permanently in the db?
+        #get its port from the db
+        laserName=sys.argv[1]
+        x=sys.argv[2]
+        y=sys.argv[3]
+    else:
+        print("NOT EXECUTED. Wrong number of arguments.  Correct usage is:")
+        print("   ./aimAtCentralMembrane.py [axis] [x] [y]")
+        sys.exit()
+    #if wrong arguments, exit with explanation
+
+    aimAtCentralMembrane(laserName,x,y)
---
+++ bin/drawWords.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+# This is designed to tell a particular egg to draw a series of line segments to spell words on the CM
+
+from aimAtCentralMembrane import aimAtCentralMembrane
+import sys
+import math
+import time
+
+    
+
+
+def drawWords(laserName,t=1,steps=20):
+    #t is the wait time in seconds.
+
+    
+    Dave={'start':[
+        (0, 0), (0, 2), (0.5, 2), (1, 1.6),
+        (1, 0.4), (0.5, 0), (1.5, 0), (2, 2),
+        (2, 2), (2.5, 0), (1.75, 1), (2.25, 1),
+        (2.5, 2), (3, 0), (3, 0), (3.5, 2),
+        (4, 2), (4, 0), (4, 0), (5, 0),
+        (5, 1), (4, 1), (5, 2), (4, 2)
+        ],
+        'end':[
+        (0, 2), (0.5, 2), (1, 1.6), (1, 0.4),
+        (0.5, 0), (0, 0), (2, 2), (2.5, 0),
+        (1.75, 1), (2.25, 1), (3, 0), (3.5, 2),
+        (4, 0), (5, 0), (4, 1), (5, 1),
+        (4, 2), (4, 2), (5, 2), (5, 2)
+        ]
+        }
+    #add offset, add scale
+    scale=100
+    shift=(-400,300)
+    #todo:  add rotation offset?
+    #todo:  make steps have a target stepsize instead of fixed number
+    for s0,e0 in zip(Dave['start'],Dave['end']):
+        s0[0]=s0[0]*scale+shift[0]
+        s0[1]=s0[1]*scale+shift[1]
+        e0[0]=e0[0]*scale+shift[0]
+        e0[1]=e0[1]*scale+shift[1]
+        step = ((e0[0] - s0[0])/steps, (e0[1] - s0[1])/steps)
+        for i in range(steps):
+            aimAtCentralMembrane(laserName, s0[0]+step[0]*i, s0[1]+step[1]*i)
+            time.sleep(t)
+    return
+  
+
+
+
+
+
+if __name__ == "__main__":
+    #check args
+    if len(sys.argv)==4:
+        laserName=sys.argv[1]
+        t=sys.argv[2]
+        steps=sys.argv[3]
+    else:
+        print("NOT EXECUTED. Wrong number of arguments.  Correct usage is:")
+        print("   ./drawWords.py [axis] [t] [steps]")
+        sys.exit()
+    #if wrong arguments, exit with explanation
+
+    drawWords(laserName,t,steps)
