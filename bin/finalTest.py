f12a32bb0d52284e41b4e7f4f0472cf5d016f25c
Seth Howell <64179490+sothcode@users.noreply.github.com>
2024-11-01 11:14:06 -0400

    Update finalTest.py
    needed sleeptime

--- bin/finalTest.py
+++ bin/finalTest.py
@@ -2,9 +2,11 @@
 
 import sys
 import os
+import time
 from aimAt import aimAt
 from goto import goto
 
+sleeptime=5
 
 def finalTest():
 
@@ -12,11 +14,13 @@ def finalTest():
     for i in range(114, 203, 1):
         rev=i/360
         goto("9N_PH", rev)
+        time.sleep(sleeptime)
 
     aimAt("9N", "9N", 25, 203)
     for i in range(202, 113, -1):
         rev=i/360
         goto("9N_PH", rev)
+        time.sleep(sleeptime)
 
     return
 
